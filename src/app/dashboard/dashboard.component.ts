707a31e37f6973eec447699a96408a5d2c1136dd
Marcin Koz³owski <marcin.kozlowski@dgt.eu>
2023-02-12 20:50:37 +0100

    add footer, flashscore logo, and logged user info

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LoginInfoService } from '../core/services/login-info.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
+  userName:string;
+  selected: Date | null;
+  constructor(private loginInfoService: LoginInfoService) {}
+
+  ngOnInit(): void {
+    this.loginInfoService.currentUserNameMessage.subscribe(
+      (msg) => (this.userName = msg)
+    );
+  }
 
 }
